3b6825d1473e402c3dfd1097fcba8c87e0c1e32f
Sindre Sorhus <sindresorhus@gmail.com>
2018-01-03 23:35:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 const path = require('path');
+const url = require('url');
 const electron = require('electron');
 const isDev = require('electron-is-dev');
-const url = require('url');
 const node = require('./node');
 
 // TODO: Implement everything
