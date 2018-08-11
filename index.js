eb0c86e3de17ee002476dee19d0e7c55d3323218
Joe Cohen <joecohenr@gmail.com>
2018-08-11 10:32:49 -0500

    Ensure user data exists for first run

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const url = require('url');
 const electron = require('electron');
 const isDev = require('electron-is-dev');
+const makeDir = require('make-dir');
 const node = require('./node');
 
 const api = new Proxy(electron, {
@@ -148,15 +149,24 @@ exports.disableZoom = (win = activeWindow()) => {
 exports.appLaunchTimestamp = Date.now();
 
 if (is.main) {
-	exports.isFirstAppLaunch = () => {
+	const isFirstAppLaunch = () => {
 		const fs = require('fs');
-		const checkFile = path.join(api.app.getPath('userData'), '.electron-util--has-app-launched');
+		const checkFile = path.join(api.app.getPath('userData'), 'FirstRun', '.electron-util--has-app-launched');
 
 		if (fs.existsSync(checkFile)) {
 			return false;
 		}
 
-		fs.writeFileSync(checkFile, '');
+		try {
+			fs.writeFileSync(checkFile, '');
+		} catch (error) {
+			if (error.code === 'ENOENT') {
+				makeDir.sync(path.join(api.app.getPath('userData'), 'FirstRun'));
+				return isFirstAppLaunch();
+			}
+		}
 		return true;
 	};
+
+	exports.isFirstAppLaunch = isFirstAppLaunch;
 }
