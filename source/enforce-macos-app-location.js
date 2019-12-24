d9bb9a68820b442c9b3c249e1545612dad2290b7
Fabio Spampinato <spampinabio@gmail.com>
2019-12-24 16:51:28 +0100

    Ensuring the linter is happy

--- source/enforce-macos-app-location.js
+++ source/enforce-macos-app-location.js
@@ -31,16 +31,17 @@ module.exports = () => {
 	}
 
 	api.app.moveToApplicationsFolder({
-    conflictHandler: conflict => {
-      if (conflict === 'existsAndRunning') { // Can't replace the active version of the app
-        api.dialog.showMessageBoxSync({
-          type: 'error',
-          message: `Another version of ${appName} is currently running, quit it and retry.`,
-          buttons: [`Quit ${appName}`]
-        });
-        app.quit();
-      }
-      return true;
-    }
+		conflictHandler: conflict => {
+			if (conflict === 'existsAndRunning') { // Can't replace the active version of the app
+				api.dialog.showMessageBoxSync({
+					type: 'error',
+					message: `Another version of ${appName} is currently running, quit it and retry.`,
+					buttons: [`Quit ${appName}`]
+				});
+				api.app.quit();
+			}
+
+			return true;
+		}
 	});
 };
